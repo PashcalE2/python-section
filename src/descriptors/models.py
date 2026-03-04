0435e0fa963e266426582f992ae6979d29406e2b
PashcalE2 <74344847+PashcalE2@users.noreply.github.com>
2026-03-04 19:48:41 +0300

    Task 2 done

--- src/descriptors/models.py
+++ src/descriptors/models.py
@@ -9,4 +9,25 @@ class Model:
 
 
 class Field:
-    pass
+    def __init__(self, path: str) -> None:
+        self.path = path
+
+    def __get__(self, instance, owner=None):
+        paths = self.path.split(".")
+        result = instance.payload
+        for key in paths:
+            if key not in result:
+                return None
+            result = result[key]
+        return result
+
+    def __set__(self, instance, value):
+        paths = self.path.split(".")
+        target = instance.payload
+        for key in paths[:-1]:
+            if key not in target:
+                return
+                # для последнего теста
+                # target[key] = {}
+            target = target[key]
+        target[paths[-1]] = value
